1edbe406a2e2b0aaba444176a3e089adff179d6a
nathanielBellamy <nbschieber@gmail.com>
2023-10-12 06:16:02 -0700

    prepping paData fftw memory allocations

--- src/c/pa_data.h
+++ src/c/pa_data.h
@@ -10,10 +10,22 @@ typedef struct {
     SNDFILE *file;
     SF_INFO sfinfo;
     float *buffer;
-    sf_count_t index; // 
+    sf_count_t index;
     sf_count_t buffer_frames; // frames processed per callback invocation
-    fftw_plan fft_plan; // plan used to compute the FFT
-    fftw_complex *fft_out; //
+  //
+  //
+  // NOTE: 
+  // - we cannot allocate/free memory within the pa_callback (see docs - callback may be called at system interrupt level)
+  // - so we allocate what memory we need here
+    fftw_plan fft_plan_to_freq; // plan used to compute the FFT, (): time_domain -> freq_domain
+    fftw_plan fft_plan_to_time; // plan used to compute the FFT, (): freq_domain -> time_domain
+    fftw_complex *fft_out; // array storing results of fft
+                           // to be read into pa out buffer while
+                           //   - mapping back to time domain space
+                           //   - normalizing to unit values
+                           // .wav_buffer --fft_plan_to_freq--> 
+                           //   paData.fft_out --destructive, in-place, "fx" transformation--> 
+                           //     paData.fft_out --fft_plan_to_time--> pa_out
 } PA_DATA;
 
 #endif
