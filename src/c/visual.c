637694acf955d3d367f77b485c522968922a8436
nathanielBellamy <nbschieber@gmail.com>
2023-10-19 20:53:22 -0700

    animate atomicCounter

--- src/c/visual.c
+++ src/c/visual.c
@@ -9,10 +9,20 @@
 
 void *visualMain(void *visualData_) 
 {
+  int frameRate = 120;
+  int frameCounter = 0;
   VISUAL_DATA *visualData = (VISUAL_DATA *) visualData_;
   while( true )
   {
-    printf("\n%d", atomic_load(visualData->atomicCounter));
+    frameCounter += 1;
+    int val = atomic_load(visualData->atomicCounter);
+    if (frameCounter == frameRate - 1)
+    {
+      system("clear");
+    } else if (frameCounter == frameRate) {
+      printf("\n\n\n\n\n\n\n\n%d", val);
+      frameCounter = 0;
+    }
   }
   printf("\nVisual Thread Signing Off");
   pthread_exit((void *) 0);
