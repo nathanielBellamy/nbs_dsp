7cd6880648cfb71523e598e4be5ab63fe0ac0e0f
nathanielBellamy <nbschieber@gmail.com>
2023-10-23 07:09:46 -0700

    testing piecewise, 16 intervals

--- src/cpp/extern_c.cpp
+++ src/cpp/extern_c.cpp
@@ -18,8 +18,8 @@ void drawGraph(int counter)
   Draw draw;
   Settings settings;
 
-  vector<double> zeroPolynomialOfMaxDegree(4, 0);
-  vector<vector<double> > polynomialArray(4, zeroPolynomialOfMaxDegree);
+  vector<double> zeroPolynomialOfMaxDegree(1, 0);
+  vector<vector<double> > polynomialArray(16, zeroPolynomialOfMaxDegree);
 
   int counterMod;
   counterMod = counter % 1000;
@@ -27,36 +27,48 @@ void drawGraph(int counter)
   // skew = ((-1.0 * counterMod) * (counterMod - 1000))/10000;
   skew = (1.0 * counterMod) / 3000;
 
-  if ( counterMod < 250 )
-  {
+  // if ( counterMod < 250 )
+  // {
     polynomialArray.at(0).at(0) = -0.75 + skew;
     polynomialArray.at(1).at(0) = 0.5 + skew;
     polynomialArray.at(2).at(0) = -1 + skew;
     polynomialArray.at(3).at(0) = 1 + skew;
-  }
+    polynomialArray.at(4).at(0) = -0.75 + skew;
+    polynomialArray.at(5).at(0) = 0.5 + skew;
+    polynomialArray.at(6).at(0) = -1 + skew;
+    polynomialArray.at(7).at(0) = 1 + skew;
+    polynomialArray.at(8).at(0) = -0.75 + skew;
+    polynomialArray.at(9).at(0) = 0.5 + skew;
+    polynomialArray.at(10).at(0) = -1 + skew;
+    polynomialArray.at(11).at(0) = 1 + skew;
+    polynomialArray.at(12).at(0) = -0.75 + skew;
+    polynomialArray.at(13).at(0) = 0.5 + skew;
+    polynomialArray.at(14).at(0) = -1 + skew;
+    polynomialArray.at(15).at(0) = 1 + skew;
+  // }
 
-  if ( counterMod > 250 && counterMod <= 500)
-  {
-    polynomialArray.at(0).at(0) = -0.75 + 1.5 * skew;
-    polynomialArray.at(1).at(0) = 0.5 + 1.5 * skew;
-    polynomialArray.at(2).at(0) = -1 + 1.5 * skew;
-    polynomialArray.at(3).at(0) = 1 + 1.5 * skew;
-  }
+  // if ( counterMod > 250 && counterMod <= 500)
+  // {
+  //   polynomialArray.at(0).at(0) = -0.75 + 1.5 * skew;
+  //   polynomialArray.at(1).at(0) = 0.5 + 1.5 * skew;
+  //   polynomialArray.at(2).at(0) = -1 + 1.5 * skew;
+  //   polynomialArray.at(3).at(0) = 1 + 1.5 * skew;
+  // }
 
-  if ( counterMod > 500 && counterMod <= 750)
-  {
-    polynomialArray.at(0).at(0) = -0.75 + 2.0 * skew;
-    polynomialArray.at(1).at(0) = 0.5 + 2.0 * skew;
-    polynomialArray.at(2).at(0) = -1 + 2.0 * skew;
-    polynomialArray.at(3).at(0) = 1 + 2.0 * skew;
-  }
+  // if ( counterMod > 500 && counterMod <= 750)
+  // {
+  //   polynomialArray.at(0).at(0) = -0.75 + 2.0 * skew;
+  //   polynomialArray.at(1).at(0) = 0.5 + 2.0 * skew;
+  //   polynomialArray.at(2).at(0) = -1 + 2.0 * skew;
+  //   polynomialArray.at(3).at(0) = 1 + 2.0 * skew;
+  // }
 
-  if ( counterMod > 750 && counterMod <= 1000)
-  {
-    polynomialArray.at(0).at(0) = -0.75 + 2.5 * skew;
-    polynomialArray.at(1).at(0) = 0.5 + 2.5 * skew;
-    polynomialArray.at(2).at(0) = -1 + 2.5 * skew;
-    polynomialArray.at(3).at(0) = 1 + 2.5 * skew;
-  }
+  // if ( counterMod > 750 && counterMod <= 1000)
+  // {
+  //   polynomialArray.at(0).at(0) = -0.75 + 2.5 * skew;
+  //   polynomialArray.at(1).at(0) = 0.5 + 2.5 * skew;
+  //   polynomialArray.at(2).at(0) = -1 + 2.5 * skew;
+  //   polynomialArray.at(3).at(0) = 1 + 2.5 * skew;
+  // }
   draw.renderPiecewise(polynomialArray, settings);
 };
