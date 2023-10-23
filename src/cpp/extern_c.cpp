9ba1d35fa0f7a2f5c1b4751123ebc83834a98938
nathanielBellamy <nbschieber@gmail.com>
2023-10-23 07:05:49 -0700

    computing and drawing piecewise

--- src/cpp/extern_c.cpp
+++ src/cpp/extern_c.cpp
@@ -19,44 +19,44 @@ void drawGraph(int counter)
   Settings settings;
 
   vector<double> zeroPolynomialOfMaxDegree(4, 0);
-  vector<vector<double> > polynomialArray(1, zeroPolynomialOfMaxDegree);
+  vector<vector<double> > polynomialArray(4, zeroPolynomialOfMaxDegree);
 
   int counterMod;
   counterMod = counter % 1000;
   double skew;
   // skew = ((-1.0 * counterMod) * (counterMod - 1000))/10000;
-  skew = (-1.0 * counterMod) / 300;
+  skew = (1.0 * counterMod) / 3000;
 
   if ( counterMod < 250 )
   {
-    polynomialArray.at(0).at(0) = -0.75;
-    polynomialArray.at(0).at(1) = 0.5 - skew / 2.0;
-    polynomialArray.at(0).at(2) = -1;
-    polynomialArray.at(0).at(3) = 2 + skew;
+    polynomialArray.at(0).at(0) = -0.75 + skew;
+    polynomialArray.at(1).at(0) = 0.5 + skew;
+    polynomialArray.at(2).at(0) = -1 + skew;
+    polynomialArray.at(3).at(0) = 1 + skew;
   }
 
   if ( counterMod > 250 && counterMod <= 500)
   {
-    polynomialArray.at(0).at(0) = -0.25;
-    polynomialArray.at(0).at(1) = 0.5 - skew / 2.0;
-    polynomialArray.at(0).at(2) = -1;
-    polynomialArray.at(0).at(3) = 2 + skew;
+    polynomialArray.at(0).at(0) = -0.75 + 1.5 * skew;
+    polynomialArray.at(1).at(0) = 0.5 + 1.5 * skew;
+    polynomialArray.at(2).at(0) = -1 + 1.5 * skew;
+    polynomialArray.at(3).at(0) = 1 + 1.5 * skew;
   }
 
   if ( counterMod > 500 && counterMod <= 750)
   {
-    polynomialArray.at(0).at(0) = 0.25;
-    polynomialArray.at(0).at(1) = 0.5 - skew / 2.0;
-    polynomialArray.at(0).at(2) = -1;
-    polynomialArray.at(0).at(3) = 2 + skew;
+    polynomialArray.at(0).at(0) = -0.75 + 2.0 * skew;
+    polynomialArray.at(1).at(0) = 0.5 + 2.0 * skew;
+    polynomialArray.at(2).at(0) = -1 + 2.0 * skew;
+    polynomialArray.at(3).at(0) = 1 + 2.0 * skew;
   }
 
   if ( counterMod > 750 && counterMod <= 1000)
   {
-    polynomialArray.at(0).at(0) = 0.75;
-    polynomialArray.at(0).at(1) = 0.5 - skew / 2.0;
-    polynomialArray.at(0).at(2) = -1;
-    polynomialArray.at(0).at(3) = 2 + skew;
+    polynomialArray.at(0).at(0) = -0.75 + 2.5 * skew;
+    polynomialArray.at(1).at(0) = 0.5 + 2.5 * skew;
+    polynomialArray.at(2).at(0) = -1 + 2.5 * skew;
+    polynomialArray.at(3).at(0) = 1 + 2.5 * skew;
   }
   draw.renderPiecewise(polynomialArray, settings);
 };
