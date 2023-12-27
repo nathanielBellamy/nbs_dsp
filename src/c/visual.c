057e9e5bd2eb32c07177d11d2ee39049b78246ce
nathanielBellamy <nbschieber@gmail.com>
2023-12-26 21:52:27 -0800

    extract fallFunction, renaming of things

--- src/c/visual.c
+++ src/c/visual.c
@@ -45,8 +45,17 @@ int xStepCount(void* settings);
 double stepWidth(void* settings);
 double stepHeight(void* settings);
 
+double fallFunction(double t)
+// this method governs how eq graphs fall toward 0
+// inbetween reads of DFT data
+{
+  // return (1.0 - 0.8 * t);
+  // return exp(-t);
+  return 2.0 - exp( 0.3 * t );
+}
+
 #define FRAME_RATE 125 // how frequently we render
-#define LOAD_RATE 222222  // how frequently we load EQ data from the audio thread
+#define READ_RATE 191111  // how frequently we read EQ data written by audio thread
 
 void *visualMain(void *visualData_) 
 {
@@ -54,18 +63,18 @@ void *visualMain(void *visualData_)
 
   //setup STARe
   int frameCounter = -1; // we eager increment so this makes 0 is first value used by loop
-  int loadCounter = -1;
+  int readCounter = -1;
 
   VISUAL_DATA *visualData = (VISUAL_DATA *) visualData_;
   // NOTE:
-  // - values are loaded from the visual thread into bufferAtomicEq_next
+  // - values are loaded from the visual thread into bufferAtomicEq_norm
   // - they are then normalized in place
-  // - on loops when new values are not fetched, the values in bufferAtomicEq_next
+  // - on loops when new values are not fetched, the values in bufferAtomicEq_norm
   //   are incrementally shifted index-wise toward 0 values in a straight-line homotopy
   //
   // 34 = 2 * audioData->buffer_frames_d2p1
   int bufferAtomicEq_load[34] = { 0.0 };
-  double bufferAtomicEq_next[34] = { 0.0 };
+  double bufferAtomicEq_norm[34] = { 0.0 };
 
   // TODO:
   // - get and update settings from user
@@ -119,13 +128,13 @@ void *visualMain(void *visualData_)
   // init playback loop
   while( true )
   {
-    frameCounter += 1;
-    loadCounter += 1;
+    frameCounter += 1;  // counter between renders 
+    readCounter += 1;   // counter between reads
 
     // load EQ data from audio thread
     int eqSync = atomic_load( visualData->atomicEqSync );
     debug.int_ = eqSync;
-    if ( loadCounter > LOAD_RATE && eqSync == 0 ) 
+    if ( readCounter > READ_RATE && eqSync == 0 ) 
     {
       int maxMag[2] = { 10000 }; // - each time we load we need to normalize 
                                  //   all values by the max value for that channel
@@ -157,20 +166,20 @@ void *visualMain(void *visualData_)
           double res = (double)bufferAtomicEq_load[index] / maxMag[ch];
           if ( res > 1.0 )
           {
-            bufferAtomicEq_next[index] = 0.0;
+            bufferAtomicEq_norm[index] = 0.0;
           }
           else if ( res < 0.0 )
           {
-            bufferAtomicEq_next[index] = 0.0;
+            bufferAtomicEq_norm[index] = 0.0;
           }
           else
           {
-            bufferAtomicEq_next[index] = res;
+            bufferAtomicEq_norm[index] = res;
           }
         }
       }
 
-      loadCounter = 0;
+      readCounter = 0;
       atomic_store(
         visualData->atomicEqSync,
         1
@@ -180,19 +189,19 @@ void *visualMain(void *visualData_)
     if ( frameCounter == FRAME_RATE )
     {
       
-      double t = (double)loadCounter / LOAD_RATE;
+      double t = (double)readCounter / READ_RATE;
       debug.double_ = t;
 
       // prep polynomials to graph
       for (int i = 0; i < 16; i++)
       {
-        double val = (1.0 - 0.8 * t) * bufferAtomicEq_next[i + 0]; // fall towards 0 inbetween loads
+        double val = fallFunction( t ) * bufferAtomicEq_norm[i + 0]; // fall towards 0 inbetween reads
         polynomialArrayL[16-i-1][0] = val;
       }
 
       for (int i = 0; i < 16; i++)
       {
-        double val = (1.0 - 0.8 * t) * bufferAtomicEq_next[i + 16];
+        double val = fallFunction( t ) * bufferAtomicEq_norm[i + 16];
         polynomialArrayR[16-i-1][0] = val;
       }
 
